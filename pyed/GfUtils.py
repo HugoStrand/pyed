80f50adc35efc12d8109d26b02022a21c2643e98
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-02-11 19:29:38 -0500

    [gf] g4 single particle transform

---
+++ pyed/GfUtils.py
@@ -0,0 +1,22 @@
+
+""" 
+Triqs Green's function utilities
+
+Author: Hugo U. R. Strand (2018), hugo.strand@gmail.com
+"""
+# ----------------------------------------------------------------------
+
+import numpy as np
+
+# ----------------------------------------------------------------------
+def g4_single_particle_transform(g4, T):
+
+    """ Transform the two-particle Green's function according to the
+    give unitary single particle transform matrix T """
+
+    g4t = g4.copy()
+    g4t.data[:] = np.einsum('ai,bj,ck,dl,PQRijkl->PQRabcd',
+        T, T.conjugate(), T, T.conjugate(), g4.data)
+
+    return g4t
+    
