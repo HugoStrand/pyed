4defae765ea7a057225b9a6f05f45a2ca48a078b
Hugo U. R. Strand <hugo.strand@gmail.com>
2018-02-11 19:49:25 -0500

    [gf] g2 single particle transf

--- pyed/GfUtils.py
+++ pyed/GfUtils.py
@@ -9,6 +9,17 @@ Author: Hugo U. R. Strand (2018), hugo.strand@gmail.com
 import numpy as np
 
 # ----------------------------------------------------------------------
+def g2_single_particle_transform(g2, T):
+
+    """ Transform the single-particle Green's function according to the
+    give unitary single particle transform matrix T """
+
+    g2t = g2.copy()
+    g2t.from_L_G_R(T, g2, T.H)
+
+    return g2t
+    
+# ----------------------------------------------------------------------
 def g4_single_particle_transform(g4, T):
 
     """ Transform the two-particle Green's function according to the
