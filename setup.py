bdb0ce95ca4d20c90980190c1fee0dd0f80c2fcc
Hugo U. R. Strand <hugo.strand@gmail.com>
2021-01-22 16:58:58 +0100

    [setup] Adding author and url

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
 from setuptools import setup
-setup(name="pyed", packages=["pyed"])
+setup(
+    name="pyed", 
+    packages=["pyed"],
+    author="Hugo U.R. Strand",
+    url="https://github.com/HugoStrand/pyed",
+    license="GPL3",
+    )
